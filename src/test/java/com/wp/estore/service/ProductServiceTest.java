821854a416c70e933ce3a58bdd638b58a51719c0
pwankar <priyanshudevilliers@gmail.com>
2025-12-16 01:23:57 +0530

    add test cases for ProductServiceTest

--- src/test/java/com/wp/estore/service/ProductServiceTest.java
+++ src/test/java/com/wp/estore/service/ProductServiceTest.java
@@ -3,37 +3,41 @@ package com.wp.estore.service;
 import com.wp.estore.dtos.ProductDto;
 import com.wp.estore.entities.Product;
 import com.wp.estore.repositories.ProductRepository;
-import com.wp.estore.services.Impl.ProductServiceImpl;
+import com.wp.estore.services.ProductService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
 
 import static org.mockito.Mockito.*;
 
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductServiceTest {
 
-    @Mock
+    @MockitoBean
     private ProductRepository productRepository;
 
-    @Mock
+    @Autowired
     private ModelMapper mapper;
 
-    @InjectMocks
-    private ProductServiceImpl productService;
+    @Autowired
+    private ProductService productService;
 
     private Product product;
 
     @BeforeEach
     public void init() {
         product = Product.builder()
+                .productId("P1")
                 .productTitle("Sample Product")
                 .productDesc("Sample Description")
                 .price(100)
@@ -46,18 +50,22 @@ public class ProductServiceTest {
     }
 
     @Test
-    void createProductTest() {
-
+    public void createProductTest() {
         when(productRepository.save(any())).thenReturn(product);
+        ProductDto product1 = productService.createProduct(mapper.map(product, ProductDto.class));
+        Assertions.assertNotNull(product1);
+        Assertions.assertEquals("Sample Product", product1.getProductTitle());
+    }
 
-
-        // assertions: id and date assigned
-
-
-        // fields copied from DTO to entity
-
-
-        // returned DTO contains the same id
-
+    @Test
+    public void updateProductTest(){
+        String productId = "P1";
+        when(productRepository.findById(Mockito.anyString())).thenReturn(Optional.of(product));
+        when(productRepository.save(Mockito.any())).thenReturn(product);
+        ProductDto productDto = mapper.map(product, ProductDto.class);
+        productDto.setProductTitle("Updated Product");
+        ProductDto updatedProduct = productService.updateProduct(productDto, productId);
+        Assertions.assertNotNull(updatedProduct);
+        Assertions.assertEquals("Updated Product", updatedProduct.getProductTitle());
     }
 }
